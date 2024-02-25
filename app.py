714911955a26aabea863e638ac6124520560d45d
Arvind Agarwal <139535632+ArvindAgarwal1310@users.noreply.github.com>
2024-02-25 15:01:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 from fastapi import FastAPI,Request
 from views import verify,handle_message
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app=FastAPI(title="Data Dine",redoc_url=None)
 
@@ -30,3 +31,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+if(__name__=="__main__"):
+    uvicorn.run(app,host="0.0.0.0",port=8080)
