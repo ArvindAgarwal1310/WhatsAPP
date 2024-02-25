cd58331034242dafb42d2b8175558dbe254eb48f
Arvind Agarwal <139535632+ArvindAgarwal1310@users.noreply.github.com>
2024-02-25 14:20:52 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,32 @@
-from flask import Flask
-from config import load_configurations, configure_logging
-from views import webhook_blueprint
+import json
+from fastapi import FastAPI,Request
+from views import verify,handle_message
+from fastapi.middleware.cors import CORSMiddleware
 
-def create_app():
-    app = Flask(__name__)
+app=FastAPI(title="Data Dine")
 
-    # Load configurations and logging settings
-    load_configurations(app)
-    configure_logging()
+@app.get("/webhook")
+async def webhook_get(request: Request):
+    raw_body = await request.body()
+    raw_body = json.loads(raw_body)
+    print(raw_body)
+    return verify(raw_body)
 
-    # Import and register blueprints, if any
-    app.register_blueprint(webhook_blueprint)
+@app.post("/webhook")
+async def webhook_post(request: Request):
+    raw_body = await request.body()
+    raw_body=json.loads(raw_body)
+    print(raw_body)
+    return handle_message(body=raw_body)
 
-    return app
-if(__name__ == "__main__"):
-    #logging.info("Flask app started")
-    app = create_app()
-    app.run(host="0.0.0.0", port=80)
+# creating origin list
+origins = ['*']
+
+# config CORSM
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
